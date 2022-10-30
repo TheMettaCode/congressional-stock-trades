651fe01706fdc74cbea39bfff722f649a4f6826b
MettaCode <mettacode@gmail.com>
2022-10-29 20:48:28 -1000

    update after first commit

--- routes/index.js
+++ routes/index.js
@@ -1,221 +1,84 @@
-//var PORT = process.env.PORT || 8080
-// var http = require('http')
-var axios = require('axios')
-var cheerio = require('cheerio')
+var PORT = 8000;
+var axios = require('axios');
+var cheerio = require('cheerio');
 var express = require('express');
-// const { url } = require('inspector');
-
 
 const app = express();
 const router = express.Router();
 
-
-const publishers = [
+const sources = [
   {
-    name: 'AP News',
-    address: 'https://apnews.com/hub/politics',
-    base: 'https://www.apnews.com',
-    slug: 'apnews'
+    name: 'Senate Stock Disclosures',
+    address: 'https://sec.report/Senate-Stock-Disclosures',
+    base: 'https://sec.report',
+    slug: 'senate',
   },
-  {
-    name: 'USA Today',
-    address: 'https://www.usatoday.com/news/politics/',
-    base: 'https://www.usatoday.com',
-    slug: 'usatoday'
-  },
-  {
-    name: 'Propublica',
-    address: 'https://www.propublica.org/',
-    base: '',
-    slug: 'propublica'
-  },
-  // {
-  //   name: 'The Washington Post',
-  //   address: 'https://www.washingtonpost.com/politics/',
-  //   base: '',
-  //   slug: 'thewashingtonpost'
-  // },
-  {
-    name: 'New York Times',
-    address: 'https://www.nytimes.com/topic/organization/us-congress',
-    base: 'https://www.nytimes.com',
-    slug: 'nytimes'
-  },
-//  {
-//    name: 'Wall Street Journal',
-//    address: 'https://www.wsj.com/news/types/washington-wire',
-//    base: '',
-//    slug: 'wsj'
-//  },
-  {
-    name: 'Politico',
-    address: 'https://www.politico.com/congress',
-    base: '',
-    slug: 'politico'
-  }
 ]
 
-const keywords = ["election", "us congress", "capitol", "capitol hill", "gop", "dems", "republicans", "democrats", "senate", "house of representatives", "speaker of the house", "stock", "investing", "tax", "majority leader", "minority leader", "filibuster", "constitution", "vote"]
+const tradesList = [];
 
-const storyList = []
+sources.forEach(source => {
+  //    console.log(sources)
+  if (source.slug == 'senate') {
+    console.log(sources);
 
-publishers.forEach(publisher => {
-  if (publisher.slug == 'apnews') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
+    axios.get(source.address)
       .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('.FeedCard')
+        const html = response.data;
+        const $ = cheerio.load(html);
 
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).text().toLowerCase().includes(word))) {
-            const title = $(story).find('.CardHeadline').find('a').text().trim()
-            const shortDescription = $(story).find('p').text().trim()
-            const url = $(story).find('a').attr('href')
-            const imageUrl = $(story).find('img').attr('src') || ''
-            const date = $(story).find('span').last().text().split(' GMT')[0]
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
+        const transactions = $('tbody').find('tr');
 
+        transactions.each((index, tAction) => {
+
+          const thisTradeItem = {};
+
+          if (index % 2 == 0) {
+
+            thisTradeItem["filedDate"] = $(tAction).find('td').first().find('div').first().text();
+            thisTradeItem["transactionDate"] = $(tAction).find('td').first().find('div').last().text();
+            thisTradeItem["ticker"] = $(tAction).find('a:nth-child(2)').text();
+            thisTradeItem["company"] = $(tAction).find('a').first().text().trim();
+            thisTradeItem["companyUrl"] = source.base + $(tAction).find('a').first().attr('href');
+            thisTradeItem["senator"] = $(tAction).find('.ov').find('a').text().trim();
+            thisTradeItem["owner"] = $(tAction).next().find('div').last().text();
+            thisTradeItem["filingsUrl"] = source.base + $(tAction).find('.ov').find('a').attr('href');
+            thisTradeItem["disclosureId"] = $(tAction).next().find('a').first().text();
+            thisTradeItem["disclosureLink"] = source.base + $(tAction).next().find('a').first().attr('href');
+            thisTradeItem["type"] = $(tAction).next().find('div').first().text();
+            thisTradeItem["tradeAmount"] = $(tAction).next().find('td:nth-child(2)').first().text();
+
+            // console.log(thisTradeItem);
+            tradesList.push(thisTradeItem);
           }
-        })
-      })
-  } else if (publisher.slug == 'usatoday') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('.gnt_m a')
 
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).text().toLowerCase().includes(word))) {
-            const title = $(story).text().trim()
-            const shortDescription = $(story).attr('data-c-br')
-            const url = $(story).attr('href')
-            const imageUrl = "" // `${$(story).find('img').attr('srcset')}`.split('?')[0] || ''
-            const date = url.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/)[0] // $(story).find('div').last().attr('data-c-dt')
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
-
-          }
-        })
-      })
+        });
+      });
+  } else if (sources.slug == 'house') {
+    console.log('SOURCE SELECTED IS HOUSE... TBD');
+  } else {
+    console.log('NO SOURCE FOUND');
   }
-  else if (publisher.slug == 'propublica') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('.home__river .home__story')
 
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).text().toLowerCase().includes(word))) {
-            const title = $(story).find('.home__story-hed').text()
-            const shortDescription = $(story).find('.home__story-dek').text().trim()
-            const url = $(story).find('h3').find('a').attr('href')
-            const imageUrl = `${$(story).find('.lead-art').find('img').attr('srcset')}`.split('?')[0] || ''
-            const date = $(story).find('p').last().find('time').text().split(',')[0]
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
+});
 
-          }
-        })
-      })
-  } else if (publisher.slug == 'thewashingtonpost') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('article div .w-100')
+console.log('COUNT OF TRADES:' + tradesList.length);
 
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).find('.story-headline').find('h3').text().toLowerCase().includes(word))) {
-            const title = $(story).find('.story-headline').find('h3').text()
-            const shortDescription = $(story).find('.story-headline').find('p').text()
-            const url = $(story).find('div a').last().attr('href')
-            const imageUrl = `${$(story).find('.border-box').find('img').attr('src')}`.split('?src=')[1] || ''
-            const date = $(story).find('span').last().text()
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
+router.get('/senate', (req, res) => {
+  console.log(tradesList);
+  res.json(tradesList);
+});
 
-          }
-        })
-      })
-  } else if (publisher.slug == 'nytimes') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('li')
-
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).find('a').find('h2').text().toLowerCase().includes(word))) {
-            const title = $(story).find('a').find('h2').text()
-            const shortDescription = $(story).find('p').text()
-            const url = $(story).find('a').attr('href')
-            const imageUrl = $(story).find('a').find('img').attr('src').split('?')[0]
-            const date = imageUrl.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/)[0]
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
-          }
-        })
-      })
-  } else if (publisher.slug == 'wsj') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('article')
-
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).find('h2').text().toLowerCase().includes(word))) {
-            const title = $(story).find('h2').text()
-            const shortDescription = $(story).find('span').last().text()
-            const url = $(story).find('a').attr('href')
-            const imageUrl = $(story).find('img').attr('src').split('?')[0] || ''
-            const date = $(story).find('p').last().text()
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
-          }
-        })
-      })
-  } else if (publisher.slug == 'politico') {
-    console.log(publisher.name)
-    axios.get(publisher.address)
-      .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const stories = $('article')
-
-        stories.each((index, story) => {
-          if (keywords.find((word) => $(story).text().toLowerCase().includes(word))) {
-            const title = $(story).find('h3').text().trim()
-            const shortDescription = $(story).find('.subhead').text()
-            const url = $(story).find('a').attr('href')
-            const imageUrl = $(story).find('.thumb').find('div').find('a').find('img').attr('src') || ""
-            const date = url.match(/(\d{1,4}([.\-/])\d{1,2}([.\-/])\d{1,4})/)[0] // $(story).find('.timestamp').text()
-            if (title.split(' ').length > 4) { storyList.push({ index, title, shortDescription, url: publisher.base + url, source: publisher.name, slug: publisher.slug, imageUrl, date }) }
-          }
-        })
-      })
-  }
-})
-
-//router.get('/blakbonz', (req, res) => { res.json('Welcome to the US Congress News API') })
-
-
-router.get('/news', (req, res) => {
-  console.log(storyList)
-
-  res.json(storyList)
-})
+router.get('/house', (req, res) => {
+  res.render('no-page', { title: 'House trades coming soon...' });
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'US Congress News API' });
+  res.render('index', { title: 'US Congress Stock Trade API' });
 });
 
 
 module.exports = router;
 
-//app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
