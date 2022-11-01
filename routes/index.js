cfc6823ba4a0d697e16a8978e94b237b7212a22b
MettaCode <mettacode@gmail.com>
2022-10-31 20:47:12 -1000

    senate function alternate used

--- routes/index.js
+++ routes/index.js
@@ -31,50 +31,81 @@ async function getData(url) {
 
 router.get('/senate', (req, res) => {
   const senateTradesList = [];
+  // const senateSource = {
+  //   name: 'Senate Stock Disclosures',
+  //   address: 'https://sec.report/Senate-Stock-Disclosures',
+  //   base: 'https://sec.report',
+  //   slug: 'senate',
+  // };
+
   const senateSource = {
     name: 'Senate Stock Disclosures',
-    address: 'https://sec.report/Senate-Stock-Disclosures',
-    base: 'https://sec.report',
+    address: 'https://senate-stock-watcher-data.s3-us-west-2.amazonaws.com/aggregate/all_transactions.json',
+    base: '',
     slug: 'senate',
   };
 
   console.log(senateSource);
   getData(senateSource.address)
     .then(responseData => {
-      const html = responseData;
+      // const html = responseData;
       // console.log("::" + html);
-      const $ = cheerio.load(html);
+      // const $ = cheerio.load(html);
 
+      const transactions = responseData;
+      transactions.sort((a, b) => new Date(b['transaction_date']) - new Date(a['transaction_date']));
+      const originalSenateTradesList = transactions.slice(0, 200);
+      const filteredList = originalSenateTradesList.filter((value) => new Date(value['transaction_date']) <= new Date());
       const senateMap = { "api": "Congressional Stock Trades by MettaCode Developers", "chamber": "senate", "retrieved": Date() };
 
-      const transactions = $('tbody').find('tr');
-
-      transactions.each((index, tAction) => {
-
+      for (var item of filteredList) {
         const thisTradeItem = {};
+        thisTradeItem["filed-date"] = item['disclosure_date'].split('/')[2] + '-' + item['disclosure_date'].split('/')[0] + '-' + item['disclosure_date'].split('/')[1];
+        thisTradeItem["transaction-date"] = item['transaction_date'].split('/')[2] + '-' + item['transaction_date'].split('/')[0] + '-' + item['transaction_date'].split('/')[1];
+        thisTradeItem["ticker"] = item['ticker'] ?? '';
+        thisTradeItem["company"] = item['asset_description'];
+        thisTradeItem["company-url"] = '';
+        thisTradeItem["member"] = item['senator'];
+        thisTradeItem["owner"] = item['owner'] ?? '';
+        thisTradeItem["filings"] = '';
+        thisTradeItem["disclosure-id"] = '';
+        thisTradeItem["disclosure-link"] = item['ptr_link'] ?? '';
+        thisTradeItem["trade-type"] = item['type'];
+        thisTradeItem["trade-range"] = item['amount'];
+        // thisTradeItem["data-retrieved"] = Date().toString();
+        // thisTradeItem["data-retrieved-by"] = "MettaCode Developers";
 
-        if (index % 2 == 0) {
+        senateTradesList.push(thisTradeItem);
+      };
 
-          thisTradeItem["filed-date"] = $(tAction).find('td').first().find('div').first().text();
-          thisTradeItem["transaction-date"] = $(tAction).find('td').first().find('div').last().text();
-          thisTradeItem["ticker"] = $(tAction).find('a:nth-child(2)').text();
-          thisTradeItem["company"] = $(tAction).find('a').first().text().trim();
-          thisTradeItem["company-url"] = ''; // senateSource.base + $(tAction).find('a').first().attr('href');
-          thisTradeItem["member"] = 'Sen. ' + $(tAction).find('.ov').find('a').text().trim();
-          thisTradeItem["owner"] = $(tAction).next().find('div').last().text();
-          thisTradeItem["filings"] = ''; // senateSource.base + $(tAction).find('.ov').find('a').attr('href');
-          thisTradeItem["disclosure-id"] = $(tAction).next().find('a').first().text();
-          thisTradeItem["disclosure-link"] = senateSource.base + $(tAction).next().find('a').first().attr('href');
-          thisTradeItem["trade-type"] = $(tAction).next().find('div').first().text();
-          thisTradeItem["trade-range"] = $(tAction).next().find('td:nth-child(2)').first().text();
-          thisTradeItem["data-retrieved"] = Date().toString();
-          thisTradeItem["data-retrieved-by"] = "MettaCode Developers";
+      // const transactions = $('tbody').find('tr');
 
-          // console.log(thisTradeItem);
-          senateTradesList.push(thisTradeItem);
-        }
+      // transactions.each((index, tAction) => {
 
-      });
+      //   const thisTradeItem = {};
+
+      //   if (index % 2 == 0) {
+
+      //     thisTradeItem["filed-date"] = $(tAction).find('td').first().find('div').first().text();
+      //     thisTradeItem["transaction-date"] = $(tAction).find('td').first().find('div').last().text();
+      //     thisTradeItem["ticker"] = $(tAction).find('a:nth-child(2)').text();
+      //     thisTradeItem["company"] = $(tAction).find('a').first().text().trim();
+      //     thisTradeItem["company-url"] = ''; // senateSource.base + $(tAction).find('a').first().attr('href');
+      //     thisTradeItem["member"] = 'Sen. ' + $(tAction).find('.ov').find('a').text().trim();
+      //     thisTradeItem["owner"] = $(tAction).next().find('div').last().text();
+      //     thisTradeItem["filings"] = ''; // senateSource.base + $(tAction).find('.ov').find('a').attr('href');
+      //     thisTradeItem["disclosure-id"] = $(tAction).next().find('a').first().text();
+      //     thisTradeItem["disclosure-link"] = senateSource.base + $(tAction).next().find('a').first().attr('href');
+      //     thisTradeItem["trade-type"] = $(tAction).next().find('div').first().text();
+      //     thisTradeItem["trade-range"] = $(tAction).next().find('td:nth-child(2)').first().text();
+      //     // thisTradeItem["data-retrieved"] = Date().toString();
+      //     // thisTradeItem["data-retrieved-by"] = "MettaCode Developers";
+
+      //     // console.log(thisTradeItem);
+      //     senateTradesList.push(thisTradeItem);
+      //   }
+
+      // });
       // console.log(senateTradesList);
       senateMap["retreived-trade-count"] = senateTradesList.length;
       senateMap["senate-trades"] = senateTradesList;
@@ -106,18 +137,18 @@ router.get('/house', (req, res) => {
         const thisTradeItem = {};
         thisTradeItem["filed-date"] = item['disclosure_date'].split('/')[2] + '-' + item['disclosure_date'].split('/')[0] + '-' + item['disclosure_date'].split('/')[1];
         thisTradeItem["transaction-date"] = item['transaction_date'];
-        thisTradeItem["ticker"] = item['ticker'];
+        thisTradeItem["ticker"] = item['ticker'] ?? '';
         thisTradeItem["company"] = item['asset_description'];
         thisTradeItem["company-url"] = '';
         thisTradeItem["member"] = item['representative'];
         thisTradeItem["owner"] = item['owner'] ?? '';
         thisTradeItem["filings"] = '';
         thisTradeItem["disclosure-id"] = '';
-        thisTradeItem["disclosure-link"] = item['ptr_link'];
+        thisTradeItem["disclosure-link"] = item['ptr_link'] ?? '';
         thisTradeItem["trade-type"] = item['type'];
         thisTradeItem["trade-range"] = item['amount'];
-        thisTradeItem["data-retrieved"] = Date().toString();
-        thisTradeItem["data-retrieved-by"] = "MettaCode Developers";
+        // thisTradeItem["data-retrieved"] = Date().toString();
+        // thisTradeItem["data-retrieved-by"] = "MettaCode Developers";
 
         houseTradesList.push(thisTradeItem);
       };
